679b86978f4aed2e59e20bbf845961ec99273497
Ygnas <ygnas@outlook.com>
2024-02-06 18:08:14 +0000

    user model

---
+++ lib/src/models/user.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class User with ChangeNotifier {
+  String? name;
+  String? email;
+  String? token;
+
+  User({this.name, this.email, this.token});
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      name: json['name'],
+      email: json['email'],
+      token: json['token'],
+    );
+  }
+}
