608f04f6cf488ab509853c8df78a57c9b396f574
Ygnas <ygnas@outlook.com>
2024-02-09 17:15:00 +0000

    Extracted login/register widgets

--- lib/src/screens/register/register.dart
+++ lib/src/screens/register/register.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:food_log/src/providers/user_provider.dart';
-import 'package:go_router/go_router.dart';
+import 'package:food_log/src/widgets/register_form.dart';
 import 'package:provider/provider.dart';
 
 class RegisterScreen extends StatelessWidget {
@@ -19,133 +19,12 @@ class RegisterScreen extends StatelessWidget {
 
     return Scaffold(
         appBar: AppBar(),
-        body: Form(
-            key: formKey,
-            child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  const Row(
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Text('Register', style: TextStyle(fontSize: 20)),
-                      )
-                    ],
-                  ),
-                  Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Row(
-                        children: [
-                          Expanded(
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Column(
-                                children: [
-                                  TextFormField(
-                                    validator: (value) {
-                                      if (value == null || value.isEmpty) {
-                                        return 'Please enter a username';
-                                      }
-                                      return null;
-                                    },
-                                    controller: usernameController,
-                                    decoration: const InputDecoration(
-                                      prefixIcon:
-                                          Icon(Icons.account_circle_outlined),
-                                      labelText: 'Username',
-                                      border: OutlineInputBorder(),
-                                    ),
-                                  ),
-                                  const Padding(padding: EdgeInsets.all(8.0)),
-                                  TextFormField(
-                                    validator: (value) {
-                                      if (value == null || value.isEmpty) {
-                                        return 'Please enter an email';
-                                      }
-                                      if (!userProvider.isEmail(value)) {
-                                        return 'Please enter a valid email';
-                                      }
-                                      return null;
-                                    },
-                                    controller: emailController,
-                                    decoration: const InputDecoration(
-                                      prefixIcon: Icon(Icons.email_outlined),
-                                      labelText: 'Email',
-                                      border: OutlineInputBorder(),
-                                    ),
-                                  ),
-                                  const Padding(padding: EdgeInsets.all(8.0)),
-                                  TextFormField(
-                                    validator: (value) {
-                                      if (value == null || value.isEmpty) {
-                                        return 'Please enter a password';
-                                      }
-                                      return null;
-                                    },
-                                    controller: passwordController,
-                                    obscureText: true,
-                                    decoration: const InputDecoration(
-                                      prefixIcon:
-                                          Icon(Icons.lock_open_outlined),
-                                      labelText: 'Password',
-                                      border: OutlineInputBorder(),
-                                    ),
-                                  ),
-                                  const Padding(padding: EdgeInsets.all(8.0)),
-                                  TextFormField(
-                                    validator: (value) {
-                                      if (value == null || value.isEmpty) {
-                                        return 'Please repeat the password';
-                                      }
-                                      if (value != passwordController.text) {
-                                        return 'Passwords do not match';
-                                      }
-                                      return null;
-                                    },
-                                    controller: confirmPasswordController,
-                                    obscureText: true,
-                                    decoration: const InputDecoration(
-                                      prefixIcon:
-                                          Icon(Icons.lock_open_outlined),
-                                      labelText: 'Repeat Password',
-                                      border: OutlineInputBorder(),
-                                    ),
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: ElevatedButton(
-                                      onPressed: () async {
-                                        if (!formKey.currentState!.validate()) {
-                                          return;
-                                        }
-                                        userProvider
-                                            .register(
-                                                usernameController.text,
-                                                emailController.text,
-                                                passwordController.text,
-                                                confirmPasswordController.text)
-                                            .then((response) {
-                                          if (response.statusCode == 200) {
-                                            context.go("/login");
-                                          } else {
-                                            ScaffoldMessenger.of(context)
-                                                .showSnackBar(SnackBar(
-                                                    content: Text(response.body
-                                                        .toString())));
-                                          }
-                                        });
-                                      },
-                                      child: const Text('Register'),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          )
-                        ],
-                      ))
-                ],
-              ),
-            )));
+        body: RegisterForm(
+            formKey: formKey,
+            usernameController: usernameController,
+            userProvider: userProvider,
+            emailController: emailController,
+            passwordController: passwordController,
+            confirmPasswordController: confirmPasswordController));
   }
 }
---
+++ lib/src/widgets/register_form.dart
@@ -0,0 +1,150 @@
+import 'package:flutter/material.dart';
+import 'package:food_log/src/providers/user_provider.dart';
+import 'package:go_router/go_router.dart';
+
+class RegisterForm extends StatelessWidget {
+  const RegisterForm({
+    super.key,
+    required this.formKey,
+    required this.usernameController,
+    required this.userProvider,
+    required this.emailController,
+    required this.passwordController,
+    required this.confirmPasswordController,
+  });
+
+  final GlobalKey<FormState> formKey;
+  final TextEditingController usernameController;
+  final UserProvider userProvider;
+  final TextEditingController emailController;
+  final TextEditingController passwordController;
+  final TextEditingController confirmPasswordController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+        key: formKey,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const Row(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text('Register', style: TextStyle(fontSize: 20)),
+                  )
+                ],
+              ),
+              Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            children: [
+                              TextFormField(
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'Please enter a username';
+                                  }
+                                  return null;
+                                },
+                                controller: usernameController,
+                                decoration: const InputDecoration(
+                                  prefixIcon:
+                                      Icon(Icons.account_circle_outlined),
+                                  labelText: 'Username',
+                                  border: OutlineInputBorder(),
+                                ),
+                              ),
+                              const Padding(padding: EdgeInsets.all(8.0)),
+                              TextFormField(
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'Please enter an email';
+                                  }
+                                  if (!userProvider.isEmail(value)) {
+                                    return 'Please enter a valid email';
+                                  }
+                                  return null;
+                                },
+                                controller: emailController,
+                                decoration: const InputDecoration(
+                                  prefixIcon: Icon(Icons.email_outlined),
+                                  labelText: 'Email',
+                                  border: OutlineInputBorder(),
+                                ),
+                              ),
+                              const Padding(padding: EdgeInsets.all(8.0)),
+                              TextFormField(
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'Please enter a password';
+                                  }
+                                  return null;
+                                },
+                                controller: passwordController,
+                                obscureText: true,
+                                decoration: const InputDecoration(
+                                  prefixIcon: Icon(Icons.lock_open_outlined),
+                                  labelText: 'Password',
+                                  border: OutlineInputBorder(),
+                                ),
+                              ),
+                              const Padding(padding: EdgeInsets.all(8.0)),
+                              TextFormField(
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'Please repeat the password';
+                                  }
+                                  if (value != passwordController.text) {
+                                    return 'Passwords do not match';
+                                  }
+                                  return null;
+                                },
+                                controller: confirmPasswordController,
+                                obscureText: true,
+                                decoration: const InputDecoration(
+                                  prefixIcon: Icon(Icons.lock_open_outlined),
+                                  labelText: 'Repeat Password',
+                                  border: OutlineInputBorder(),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: ElevatedButton(
+                                  onPressed: () async {
+                                    if (!formKey.currentState!.validate()) {
+                                      return;
+                                    }
+                                    userProvider
+                                        .register(
+                                            usernameController.text,
+                                            emailController.text,
+                                            passwordController.text,
+                                            confirmPasswordController.text)
+                                        .then((response) {
+                                      response.statusCode == 200
+                                          ? context.go("/login")
+                                          : ScaffoldMessenger.of(context)
+                                              .showSnackBar(const SnackBar(
+                                                  content: Text(
+                                                      "Something went wrong. Please try again.")));
+                                    });
+                                  },
+                                  child: const Text('Register'),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  ))
+            ],
+          ),
+        ));
+  }
+}
