695576f2f5caa2b9107edc4c66116c5c07c02a99
Ygnas <ygnas@outlook.com>
2024-02-21 16:34:29 +0000

    Shows comment widget
    Made whole screen as scroll view add padding

--- lib/src/screens/listings/listings.dart
+++ lib/src/screens/listings/listings.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:food_log/src/models/listing.dart';
+import 'package:food_log/src/widgets/comments.dart';
+import 'package:food_log/src/widgets/like_commnet.dart';
 
 class ListingScreen extends StatelessWidget {
   final Listing listing;
@@ -11,7 +13,7 @@ class ListingScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Listing'),
       ),
-      body: Center(
+      body: SingleChildScrollView(
         child: Column(
           children: [
             ListTile(
@@ -38,6 +40,27 @@ class ListingScreen extends StatelessWidget {
                 ),
               ),
             ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  LikeComment(
+                    data: listing.likes.length,
+                    dataName: '',
+                    icon: Icons.thumb_up_off_alt_outlined,
+                  ),
+                  const SizedBox(width: 16.0),
+                  LikeComment(
+                    data: listing.comments.length,
+                    dataName: 'comments',
+                    icon: Icons.mode_comment_outlined,
+                  ),
+                ],
+              ),
+            ),
+            listing.comments.isNotEmpty
+                ? Comments(listing: listing)
+                : const SizedBox(),
           ],
         ),
       ),
